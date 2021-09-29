00f230efc9af5af42f73d50421a7482f042fbd80
NiCrOmAnT <61577550+NiCrOmAnT@users.noreply.github.com>
2021-09-29 20:57:58 +0300

    Добавил тесты

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const epsilon = 0.00000000001;
+const epsilon = 0.00000000001
+
+const triangleType = {
+    equilateral: 'equilateral',
+    error: 'error',
+    isosceles: 'isosceles',
+    common: 'common',
+    notTriangle: 'not_a_triangle'    
+}
 
 const a = process.argv[2]
 const b = process.argv[3]
@@ -6,28 +14,29 @@ const c = process.argv[4]
 
 function defineTriangle(a, b, c) {
     if (!isNumber(a) || !isNumber(b) || !isNumber(c) || a < epsilon || b < epsilon || c < epsilon) {
-        return 'error'
+        return triangleType.error
     }
     a = Number(a)
     b = Number(b)
     c = Number(c)
     if (a > Number.MAX_SAFE_INTEGER || b > Number.MAX_SAFE_INTEGER || c > Number.MAX_SAFE_INTEGER) {
-        return 'error'
+        return triangleType.error
     }
     if (a <= 0 || b <= 0 || c <= 0) {
-        return 'error'
+        return triangleType.error
     }
     if (equal(a, b) && equal(a, c)) {
-        return 'equilateral'
+        return triangleType.equilateral
     }
 
     if (greaterThen(a + b, c) && greaterThen(a + c, b) && greaterThen(b + c, a)) {
         if (equal(a, b) || equal(a, c) || equal(b, c)) {
-            return 'isosceles'
+            return triangleType.isosceles
         }
-        return 'common'
+        return triangleType.common
     }
-    return 'not_a_triangle'
+    
+    return triangleType.notTriangle
 }
 
 function greaterThen(A, B) {
@@ -44,4 +53,7 @@ function isNumber(a) {
 
 defineTriangle(a, b, c)
 
-module.exports = defineTriangle;
+module.exports = {
+    defineTriangle,
+    triangleType,
+}
